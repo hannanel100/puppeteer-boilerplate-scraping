f63066e366eb7624ec939ec2303dcf25dfae3bcb
hannanel gershinsky <hannanel100@gmail.com>
2020-12-16 11:32:53 +0200

    Currently scraping one spec to console.

--- pageScraper.js
+++ pageScraper.js
@@ -1,19 +1,26 @@
 const scraperObject = {
   //Change url to link you're scraping from
-  url: 'https://www.imdb.com/name/nm6289892/',
-  async scraper(browser){
-      let page = await browser.newPage();
-      console.log(`Navigating to ${this.url}...`);
-      await page.goto(this.url);
-      await page.waitForTimeout(5000)
-      //Enter following code here
-
-
-
-      //Program successfully completed
-      await browser.close();
-      console.log('Program completed!')
-  }
-}
+  url: "https://global.ihs.com/search_res.cfm?&rid=IHS&input_doc_number=",
+  async scraper(browser, specToRequest) {
+    let page = await browser.newPage();
+    console.log(`Navigating to ${this.url}${specToRequest}...`);
+    await page.goto(this.url + specToRequest);
+    await page.waitForTimeout(5000);
+    //Enter following code here
+    //TODO: Add switch statement dealing with other spec publishers, currently works only for AMS
+    const currentRevisionElement = await page.$eval(
+      "body > table > tbody > tr:nth-child(1) > td > table > tbody > tr:nth-child(5) > td > table > tbody > tr > td > form > div:nth-child(3) > div.search-result-detail-container > span > span",
+      (el) => el.innerText
+    );
+    const currentRevision = currentRevisionElement
+      .trim()
+      .split(" ")[1]
+      .split(",")[0];
+    console.log(currentRevision);
+    //Program successfully completed
+    await browser.close();
+    console.log("Program completed!");
+  },
+};
 
 module.exports = scraperObject;
